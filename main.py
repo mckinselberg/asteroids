7eea3c16905f4c82337eb5a3812e8f222c997e0f
Dan <dan@dankinsley.com>
2024-10-27 21:27:28 -0400

    add shapes and player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -7,13 +8,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        dt = clock.tick(fps)/1000
         pygame.display.flip()
+        # print(dt)
 
 if __name__ == '__main__':
     main()
