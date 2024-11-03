6a94758b6ff49310267df738884f591e9a40af1d
Dan <dan@dankinsley.com>
2024-11-03 16:44:40 -0500

    flyable ship, spawning asteroids

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     dt = 0
     fps = 60
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        dt = clock.tick(fps)/1000
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
-        # print(dt)
+
+        dt = clock.tick(fps)/1000
 
 if __name__ == '__main__':
     main()
